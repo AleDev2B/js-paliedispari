deef4486e62cbdf32f8685dbfadd59690d9d9665
Alessandro <alecritelli@gmail.com>
2020-03-31 01:02:29 +0200

    fix. msg &logic + add:layout (color -page) +

--- ex.2/js/myscript.js
+++ ex.2/js/myscript.js
@@ -5,30 +5,36 @@ Generiamo un numero random (sempre da 1 a 5) per il computer.
 */
 
 // definzione variabili globali
-var RandNum1,RandNum2,TotalRandNum;
+var Num1,Num2,TotalRandNum;
 // dichiarare all'utente se sceglie pari o dispari
 var BetUser1 = prompt("scegli pari o dispari")
-
+document.getElementById("Userdecl").innerHTML = "Il giocatore ha scelto:" + BetUser1;
 /*far generare al computer un primo numero random e memorizzarlo nella var1
 far generare al computer un secondo numero random e memorizzarlo nella var2
 creazione funzione randomize*/
 
-function Randomize(RandNum1,RandNum2) {
-  var RandNum1 = parseInt(Math.random() * 5);
-  var RandNum2 = parseInt(Math.floor(Math.random() * 5));
-  return RandNum1,RandNum2;
+function Randomize (){
+   Num1 = parseInt(Math.random() * 5)
+   Num2 = parseInt(Math.floor(Math.random() * 5))
+  return Num1,Num2;
 }
 
+// verifica
+console.log(Randomize(Num1,Num2));
 
+// stampa a video dei due numeri estratti randomicamente
+document.getElementById("firstnum").innerHTML = "Il primo numero estratto è il:" + Num1;
+document.getElementById("secondnum").innerHTML = "Il secondo numero estratto è il:" + Num2;
 // // fare la somma dei due numeri generati
 // generare la funzione Sum
-function Sum(RandNum1,RandNum2) {
-  var TotalRandNum = RandNum1 + RandNum2;
+function Sum() {
+   TotalRandNum = Num1 + Num2;
   return TotalRandNum;
 }
 
+console.log(Sum(Num1,Num2));
 // // definire la condizione di pari e dispari per la somma dei due numeri generati dal computer e confrontala con la selezione scelta dall'utente
-if (TotalRandNum%2 == 0 && BetUser1 == "pari") {
+if ((TotalRandNum%2 == 0) && BetUser1 == "pari") {
   document.getElementById('decl').innerHTML = "hai vinto alla grande"
 }
 else {
