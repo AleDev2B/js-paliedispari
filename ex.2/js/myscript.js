abf5ae90dec9a4b4ec5739cf64d245b221aebef1
Alessandro <alecritelli@gmail.com>
2020-03-31 01:09:02 +0200

    fix. comments

--- ex.2/js/myscript.js
+++ ex.2/js/myscript.js
@@ -6,34 +6,35 @@ Generiamo un numero random (sempre da 1 a 5) per il computer.
 
 // definzione variabili globali
 var Num1,Num2,TotalRandNum;
-// dichiarare all'utente se sceglie pari o dispari
+
+// msg all'utente di scelta tra pari o dispari
 var BetUser1 = prompt("scegli pari o dispari")
 document.getElementById("Userdecl").innerHTML = "Il giocatore ha scelto:" + BetUser1;
-/*far generare al computer un primo numero random e memorizzarlo nella var1
-far generare al computer un secondo numero random e memorizzarlo nella var2
-creazione funzione randomize*/
 
+// creazione funzione randomize numbers
 function Randomize (){
-   Num1 = parseInt(Math.random() * 5)
-   Num2 = parseInt(Math.floor(Math.random() * 5))
+   Num1 = parseInt(Math.random() * 5)  // far generare al computer un primo numero random e memorizzarlo nella var1
+   Num2 = parseInt(Math.floor(Math.random() * 5))  // far generare al computer un secondo numero random e memorizzarlo nella var2
   return Num1,Num2;
 }
 
-// verifica
+// verifica dell'oprazione di randomizzazione
 console.log(Randomize(Num1,Num2));
 
 // stampa a video dei due numeri estratti randomicamente
 document.getElementById("firstnum").innerHTML = "Il primo numero estratto è il:" + Num1;
 document.getElementById("secondnum").innerHTML = "Il secondo numero estratto è il:" + Num2;
-// // fare la somma dei due numeri generati
-// generare la funzione Sum
+
+
+// creazione della funzione Sum tra i due numeri Random creati dal computer
 function Sum() {
-   TotalRandNum = Num1 + Num2;
+   TotalRandNum = Num1 + Num2;  // dichiarazione dell'operazione di somma tra due numeri
   return TotalRandNum;
 }
-
+// verifica dell'oprazione di somma
 console.log(Sum(Num1,Num2));
-// // definire la condizione di pari e dispari per la somma dei due numeri generati dal computer e confrontala con la selezione scelta dall'utente
+
+// definizione della condizione di pari e dispari per il totale della somma dei numeri (generati dal computer) e confrontarla con la selezione scelta dall'utente per stabilire se ha vinto o perso
 if ((TotalRandNum%2 == 0) && BetUser1 == "pari") {
   document.getElementById('decl').innerHTML = "hai vinto alla grande"
 }
