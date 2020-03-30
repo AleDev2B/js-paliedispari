d9b719d3867ba72bc32f6cea0c7754fce3897a79
Alessandro <alecritelli@gmail.com>
2020-03-31 00:26:25 +0200

    final fix

--- ex.1/js/myscript.js
+++ ex.1/js/myscript.js
@@ -7,7 +7,7 @@ qui sicuramente da cercare ci sarà il come revertare una parola.*/
 // creo un array vuoto
 var ArrayWord = [];
 // dichiaro le var globali
-var ReverseArray, JointArray, Wordsplit
+var ReverseArray, JointArray, Wordsplit, UserWord
 // Chiedo all'utente di inserire come input una parola
 var UserWord = prompt("Ciao Frenzo, inserisci una parola magica")
 console.log(UserWord);
@@ -15,15 +15,17 @@ console.log(UserWord);
 // creo la funzione stringa inversa
 function reverseString() {
 
-  var Wordsplit = UserWord.split("") // divido la parola in lettere e la salvo in una variabile
+  Wordsplit = UserWord.split("") // divido la parola in lettere e la salvo in una variabile
 
   ArrayWord.push(Wordsplit) // prendo la parola e la inserisco nell'array vuoto
 
   ReverseArray = Wordsplit.reverse() // uso il metodo reverse per modificare l'array con le stringhe inserite
 
   JointArray = ReverseArray.join("") // uso il metodo join per unire tutti gli elementi dell'array modificato in una sola stringa
+  return JointArray;
 }
 
+reverseString(UserWord);
 
 document.getElementById('parola').innerHTML = "Questa è la parola indicata dall'utente:" + UserWord;
 document.getElementById('parola2').innerHTML = "La parola scelta è stata suddivisa in singole lettere:"+ ArrayWord;
